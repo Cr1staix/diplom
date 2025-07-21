e954befc9aacaea0148739b20a9947b629e274dc
Алексей <sayhell042@gmail.com>
2025-07-21 19:15:46 +0700

    FEAT: (CUM-6) added crud for user entity

--- src/main/java/ru/top/diplom/controller/UserController.java
+++ src/main/java/ru/top/diplom/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.top.diplom.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/ru/top/diplom/service/UserService.java
+++ src/main/java/ru/top/diplom/service/UserService.java
@@ -1,7 +1,6 @@
 package ru.top.diplom.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.top.diplom.dto.userDTO.UserCreateDTO;
 import ru.top.diplom.dto.userDTO.UserResponseDTO;
