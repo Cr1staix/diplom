c385af16fd7464cf56b260cc30589fa8caa27607
Алексей <sayhell042@gmail.com>
2025-07-21 19:15:28 +0700

    FEAT: (CUM-6) added crud for user entity

---
+++ src/main/java/ru/top/diplom/controller/UserController.java
@@ -0,0 +1,58 @@
+package ru.top.diplom.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.top.diplom.dto.userDTO.UserCreateDTO;
+import ru.top.diplom.dto.userDTO.UserResponseDTO;
+import ru.top.diplom.dto.userDTO.UserUpdateDTO;
+import ru.top.diplom.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody UserCreateDTO userCreateDTO) {
+
+        return ResponseEntity.ok(userService.create(userCreateDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id){
+
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<UserResponseDTO> update (@RequestBody UserUpdateDTO userUpdateDTO){
+
+        return ResponseEntity.ok(userService.update(userUpdateDTO));
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<String> delete (@PathVariable Long id){
+
+        userService.delete(id);
+
+        return ResponseEntity.ok("Пользователь с id : " + id + " удалён" );
+    }
+}
---
+++ src/main/java/ru/top/diplom/exception/UserAllreadyExistsException.java
@@ -0,0 +1,9 @@
+package ru.top.diplom.exception;
+
+public class UserAllreadyExistsException extends RuntimeException {
+    private static final String MESSAGE = "Пользователь с телефоном %s уже зарегистрирован";
+
+    public UserAllreadyExistsException(String phone) {
+        super(MESSAGE.formatted(phone));
+    }
+}
